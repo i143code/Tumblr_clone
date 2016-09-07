9f96e8686b0fd79c632d07d873b740da69ab9dc2
i143code <ranjanah143code@yahoo.com>
2016-09-07 18:29:47 -0400

    adding edit aand delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,32 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	# edit and upate
+	def edit
+		@post = Post.find(params[:id])
+
+	end
+
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(post_params)
+			flash[:notice] ="Article has been saved"
+			redirect_to @post
+	    else
+	    	render 'edit'
+
+	    end
+
+
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to root_path
+	end
+
 
 private 
     def post_params
